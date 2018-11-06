59d86345e23472fc0e94d715475bc7abf39cc7e0
Harshit Shetty <swaharsh@gmail.com>
2018-11-07 00:56:33 +0530

    Add [Question Model Sample Array]

--- src/screens/Questions.js
+++ src/screens/Questions.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
+import _ from 'lodash'; 
 
 class QuestionList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { questions: [
+            {
+                type : 'mcq',
+                id: 1,
+                text: 'What is the capita of India?',
+                image: 'null',
+                options: [
+                    'Mumbai',
+                    'Bangalore',
+                    'Chennai',
+                    'New Delhi'
+                ]
+            },
+            {
+                type : 'audio',
+                id: 2,
+                text: 'What are your hobbies?',
+                image: 'null',
+                options: {
+                    maxLength : '30'
+                }
+            }
+        ] };
+
+     }
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Questions</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={() => this.props.navigation.navigate('Details')}
-                />
-                <Button
-                    title="Go to Record"
-                    onPress={() => this.props.navigation.navigate('Record')}
-                />
+                {_.map(this.state.questions, 
+                    question => {
+                        if(question.type == 'mcq'){
+                            <Button
+                                title="Answer this question"
+                                onPress={() => this.props.navigation.navigate('Record',
+                                {
+                                    text : question.text,
+                                    options: question.options        
+                                })}
+                            />
+                        }
+                        else if (question.type == 'audio'){
+                            <Button
+                                title="Record this question"
+                                onPress={() => this.props.navigation.navigate('Record')}
+                            />
+                        }
+                    }
+                )}
+
+
             </View>
         );
     }
