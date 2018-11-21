ba3921ac5cf86d954268cbc27750f85a1f19ed84
Harshit Shetty <swaharsh@gmail.com>
2018-11-22 00:01:44 +0530

    Add [CheckBox]

--- src/screens/MCQ.js
+++ src/screens/MCQ.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
 import _ from 'lodash';
-import { global } from '../global';
+import CheckBox from 'react-native-check-box'
+import '../global.js';
 
 class MCQ extends React.Component {
 
-    saveAndExit  = () => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isChecked : false
+            }
+        }
+
+
+    saveAndExit  = (id, ans) => {
+        // global.answer = ans;
         this.props.navigation.navigate('Questions')
     }
 
@@ -15,19 +25,33 @@ class MCQ extends React.Component {
         const questionId = navigation.getParam('id', 'NO-ID');
         const questionTitle = navigation.getParam('text', 'NO-ID');
         const questionOptions = navigation.getParam('options', 'default');
+        let answer = '';
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>{ questionTitle }</Text>
                 <Text>Options</Text>
                 {_.map(questionOptions,
                     option => {
-                        return <Text>{ option }</Text>
+                        return <React.Fragment>
+                            <CheckBox
+                                style={{ width : 170 }}
+                                onClick={() => {
+                                    this.setState({
+                                        isChecked: !this.state.isChecked
+                                    });
+                                    answer = { option }
+                                }}
+                                isChecked={this.state.isChecked}
+                                leftText={ option }
+                                
+                            />
+                        </React.Fragment>
                         
                     }
                 )}
                 <Button
                     title="Save"
-                    onPress={() => this.saveAndExit()}
+                    onPress={() => this.saveAndExit(questionId, answer)}
                 />
 
 
